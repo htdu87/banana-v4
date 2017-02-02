c6c043693f03d22cec85125e6326c9dd606048c3
htdu87 <huynhthanhdu@gmail.com>
2017-02-02 21:25:36 +0700

    Add user class

--- core/user.php
+++ core/user.php
@@ -5,6 +5,13 @@
 		private $name;
 		private $email;
 		private $rid;
+		private $phone;
+		private $avatar;
+		private $active;
+		private $lock;
+		private $vip;
+		private $regdate;
+		private $lastlogin;
 		
 		function __construct(){
 			$this->usr = "";
@@ -29,6 +36,24 @@
 		public function set_rid($rid){
 			$this->rid = $rid;
 		}
+		public function set_phone($phone){
+			$this->phone=$phone;
+		}
+		public function set_avatar($avatar){
+			$this->avatar=$avatar;
+		}
+		public function set_lock($lock){
+			$this->lock=$lock;
+		}
+		public function set_vip($vip){
+			$this->vip=$vip;
+		}
+		public function set_regdate($regdate){
+			$this->regdate=$regdate;
+		}
+		public function set_lastlogin($lastlogin){
+			$this->lastlogin=$lastlogin;
+		}
 		
 		public function get_username(){
 			return $this->usr;
@@ -44,6 +69,24 @@
 		}
 		public function get_rid(){
 			return $this->rid;
+		}
+		public function get_phone(){
+			return $this->phone;
+		}
+		public function get_avatar(){
+			return $this->avatar;
+		}
+		public function get_lock(){
+			return $this->lock;
+		}
+		public function get_vip(){
+			return $this->vip;
+		}
+		public function get_regdate(){
+			return $this->regdate;
+		}
+		public function get_lastlogin(){
+			return $this->lastlogin;
 		}
 	}
 ?>
